1103435b4b98cd3cde30918f5a3d29f2e348e324
Erick Mekubo <emekubo07@gmail.com>
2025-09-11 15:08:36 +0300

    ML-AI and Ollama Integration and Advanced Features 4

--- wizard/__init__.py
+++ wizard/__init__.py
@@ -2,3 +2,4 @@ from . import composition_wizard
 from . import rebuild_client_history_wizard
 from . import wizard_models
 from . import ml_training_wizard
+from . import simplified_composition_wizard
