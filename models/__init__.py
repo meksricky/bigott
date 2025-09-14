9472e80f5b61aef60019e2c1be0094c32b997c03
Erick Mekubo <emekubo07@gmail.com>
2025-09-14 16:54:17 +0300

    use action

--- models/__init__.py
+++ models/__init__.py
@@ -2,7 +2,7 @@
 
 from . import product_template
 from . import gift_composition
-from . import client_order_history  
-from . import res_partner  
+from . import client_order_history
+from . import res_partner
 from . import ollama_gift_recommender
-from . import res_partner_ollama 
+from . import res_partner_ollama
